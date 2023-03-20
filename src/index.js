40d6532f076eebfcfe0ff5e00570dcb699c6bd30
frkn8 <103068163+frkn8@users.noreply.github.com>
2023-03-20 23:15:08 +0300

    tamamlandı...

--- src/index.js
+++ src/index.js
@@ -43,3 +43,32 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+const liste = document.querySelectorAll("nav a");
+const nesneler = Object.keys(siteContent.nav);
+for (let i = 0; i< nesneler.length; i++){
+  liste[i].textContent=siteContent.nav[nesneler[i]];
+  liste[i].classList.add("italic");
+}
+const logo = document.querySelectorAll("img");
+const fotograf = Object.keys(siteContent.images);
+for (let i = 0; i< fotograf.length; i++){
+  logo[i].src=siteContent.images[fotograf[i]];
+}
+const mycta = document.querySelectorAll(".cta-text h1, .cta-text button");
+const ctakeys = Object.keys(siteContent.cta);
+for (let i=0; i<ctakeys.length; i++){
+  mycta[i].textContent=siteContent.cta[ctakeys[i]];
+}
+const icerik = document.querySelectorAll(".main-content h4, .main-content p");
+const icerikkeys = Object.keys(siteContent["ana-içerik"]);
+for (let i=0; i<icerikkeys.length; i++){
+  icerik[i].textContent=siteContent["ana-içerik"][icerikkeys[i]];
+}
+const ulasim = document.querySelectorAll(".contact h4, .contact p");
+const ulasimkeys = Object.keys(siteContent.iletisim);
+for (let i=0; i<ulasimkeys.length; i++){
+  ulasim[i].textContent=siteContent.iletisim[ulasimkeys[i]];
+}
+const sayfasonu = document.querySelector("footer a");
+sayfasonu.textContent=siteContent.footer.copyright;
+sayfasonu.classList.add("bold");
